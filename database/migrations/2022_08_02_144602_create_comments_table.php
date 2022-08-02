e5221b29022bb0bf0a5fcb4feddb8c968b933889
salamone-gius <salamone.gius@gmail.com>
2022-08-02 16:57:26 +0200

    comments table created

---
+++ database/migrations/2022_08_02_144602_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            // aggiungo la colonna foreign key relativa alla tabella 'posts' in modo che se il post viene cancellato vengono cancellati tutti i commenti relativi
+            $table->foreignId('posts')->constrained()->onDelete('cascade');
+            // aggiungo la colonna relativa al nome dell'autore settandola nullable
+            $table->string('author', 80)->nullable();
+            // aggiungo la colonna relativa al testo del commento
+            $table->text('content');
+            // aggiungo la colonna con il valore booleano che gestirà l'approvazione o meno del commento settandolo 'false' di default
+            $table->boolean('is_approved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
